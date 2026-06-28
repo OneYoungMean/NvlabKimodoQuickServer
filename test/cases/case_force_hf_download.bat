291bbc1c1545a26a61cc37b9264b9b9b290e7e5b
OneYoungMean <3303609008@qq.com>
2026-06-28 15:59:05 +0800

    fix tst

--- test/cases/case_force_hf_download.bat
+++ test/cases/case_force_hf_download.bat
@@ -4,5 +4,7 @@ set "SCRIPT_DIR=%~dp0"
 set "RESULT_FILE=%~1"
 if not defined RESULT_FILE set "RESULT_FILE=%TEMP%\kimodo_case_force_hf_download_%RANDOM%%RANDOM%.txt"
 set "KIMODO_TEST_FORCE_HF_DOWNLOAD=1"
+if not defined KIMODO_TEST_RUN1_WAIT_TIMEOUT_SEC set "KIMODO_TEST_RUN1_WAIT_TIMEOUT_SEC=1800"
+if not defined KIMODO_TEST_RUN2_WAIT_TIMEOUT_SEC set "KIMODO_TEST_RUN2_WAIT_TIMEOUT_SEC=1800"
 call "%SCRIPT_DIR%case_runner.bat" "force_hf_download" "" "" "" "0" "0" "0" "%RESULT_FILE%"
 exit /b %ERRORLEVEL%
